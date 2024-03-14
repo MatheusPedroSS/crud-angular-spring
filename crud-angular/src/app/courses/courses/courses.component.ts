1c26d051e5467bfd929d6d80e979b2beb6396629
Matheus P S Sousa <matheus.pedro1214@gmail.com>
2024-03-14 20:51:22 -0300

    Adicionando o Spinner enquanto os dados não são apresentados ao usuario

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -3,23 +3,25 @@ import { AppMaterialModule } from '../../shared/app-material/app-material.module
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
 import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
   imports: [
-    AppMaterialModule
+    AppMaterialModule,
+    CommonModule
   ],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
 
-   courses: Observable<Course[]>;
+   courses$: Observable<Course[]>;
    displayedColumns = ['name', 'category']
 
    constructor(private coursesService: CoursesService) {
-    this.courses = coursesService.listAll();
+    this.courses$ = coursesService.listAll();
    }
 
 }
