608c1ac7ff5a59c1ea2a9e7cccb9b2391d9210ef
Matheus P S Sousa <matheus.pedro1214@gmail.com>
2024-03-13 21:00:58 -0300

    Ajustando rotas e incluindo o componente de table.

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
 export class CoursesComponent {
+
+   courses: Course[];
+   displayedColumns = ['name', 'category']
+
+   constructor() {
+    this.courses = [
+      {_id: '1', name: 'Angular', category: 'front-end'},
+      {_id: '2', name: 'Java', category: 'back-end'}
+    ]
+   }
 
 }
